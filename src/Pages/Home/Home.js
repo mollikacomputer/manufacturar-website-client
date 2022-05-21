8fa18a2e9f62363a9186b0b48c9750aa30cf5e94
mollikacomputer <mollikacomputer3@gmail.com>
2022-05-21 22:36:31 +0600

    carousal banner added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Banner from '../../Components/Banner';
+import CarousalBanner from './CarousalBanner';
 
 const Home = () => {
     return (
         <div>
+            <CarousalBanner/>
             <Banner/>
 
         </div>
